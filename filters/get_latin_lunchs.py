f84fb910ea98298c418a2f78816402565b3d04c7
cqb13 <cqb13.main@gmail.com>
2025-01-17 21:18:24 -0500

    initial commit

---
+++ filters/get_latin_lunchs.py
@@ -0,0 +1,25 @@
+import json
+from datetime import datetime
+
+current_date = datetime.strptime("2025-01-17", "%Y-%m-%d")
+grad_date = datetime.strptime("2025-06-08", "%Y-%m-%d")
+
+with open("../data/clean_calendar.json", "r") as file:
+    events = json.load(file)
+
+latin_lunch_days = [
+    event
+    for event in events
+    if event["Title"] == "Day F"
+    and datetime.strptime(event["Starts"].split(" ")[0], "%Y-%m-%d") > current_date
+    and datetime.strptime(event["Starts"].split(" ")[0], "%Y-%m-%d") < grad_date
+]
+
+latin_lunch_days.sort(
+    key=lambda event: datetime.strptime(event["Starts"].split(" ")[0], "%Y-%m-%d")
+)
+
+with open("../data/latin_lunch_days.json", "w") as file:
+    json.dump(latin_lunch_days, file, indent=4)
+
+print("Filtered, sorted, and saved to latin_lunch_days.json.")
---
+++ index.js
@@ -0,0 +1,223 @@
+async function startCountdown(endDate, daysID, hoursID, minutesID, secondsID) {
+  async function updateCountdown() {
+    const currentDate = new Date();
+
+    const timeDifference = new Date(endDate) - currentDate;
+
+    if (timeDifference <= 0) {
+      document.getElementById(daysID).textContent = "00";
+      document.getElementById(hoursID).textContent = "00";
+      document.getElementById(minutesID).textContent = "00";
+      document.getElementById(secondsID).textContent = "00";
+      clearInterval(countdownInterval);
+      return;
+    }
+
+    const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hoursRemaining = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutesRemaining = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60),
+    );
+    const secondsRemaining = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById(daysID).textContent = daysRemaining
+      .toString()
+      .padStart(2, "0");
+    document.getElementById(hoursID).textContent = hoursRemaining
+      .toString()
+      .padStart(2, "0");
+    document.getElementById(minutesID).textContent = minutesRemaining
+      .toString()
+      .padStart(2, "0");
+    document.getElementById(secondsID).textContent = secondsRemaining
+      .toString()
+      .padStart(2, "0");
+  }
+
+  updateCountdown();
+
+  const countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+async function loadCountdowns() {
+  try {
+    const response = await fetch("/data/clean_calendar.json");
+    const calendarData = await response.json();
+
+    // Graduation
+    const graduationEvent = calendarData.find(
+      (event) => event.Title === "SHS GRADUATION",
+    );
+
+    if (!graduationEvent) {
+      console.error("SHS Graduation event not found.");
+      return;
+    }
+
+    const graduationDate = graduationEvent.Starts;
+
+    calculateDaysOfWeekLeft(graduationDate);
+
+    startCountdown(
+      graduationDate,
+      "grad-days",
+      "grad-hours",
+      "grad-minutes",
+      "grad-seconds",
+    );
+
+    // Prom
+    const promEvent = calendarData.find(
+      (event) => event.Title === "Senior Prom",
+    );
+
+    if (!promEvent) {
+      console.error("prom event not found.");
+      return;
+    }
+
+    const promDate = promEvent.Starts;
+
+    startCountdown(
+      promDate,
+      "prom-days",
+      "prom-hours",
+      "prom-minutes",
+      "prom-seconds",
+    );
+
+    // Senior Awards
+    const awardsEvent = calendarData.find(
+      (event) => event.Title === "Senior Awards Night",
+    );
+
+    if (!awardsEvent) {
+      console.error("awards event not found.");
+      return;
+    }
+
+    const awardsDate = awardsEvent.Starts;
+
+    startCountdown(
+      awardsDate,
+      "awards-days",
+      "awards-hours",
+      "awards-minutes",
+      "awards-seconds",
+    );
+
+    // Hackathon
+    const hackathonEvent = calendarData.find(
+      (event) => event.Title === "Hackathon (with SMS students)",
+    );
+
+    if (!hackathonEvent) {
+      console.error("hackathon event not found.");
+      return;
+    }
+
+    const hackathonDate = hackathonEvent.Starts;
+
+    startCountdown(
+      hackathonDate,
+      "hackathon-days",
+      "hackathon-hours",
+      "hackathon-minutes",
+      "hackathon-seconds",
+    );
+
+    // AP Exam Week 1
+    const apEvent = calendarData.find(
+      (event) => event.Title === "AP Exams Week 1",
+    );
+
+    if (!apEvent) {
+      console.error("ap event not found.");
+      return;
+    }
+
+    const apDate = apEvent.Starts;
+
+    startCountdown(apDate, "ap-days", "ap-hours", "ap-minutes", "ap-seconds");
+
+    // Start Q4
+    const q4Event = calendarData.find((event) => event.Title === "Start Q4");
+
+    if (!q4Event) {
+      console.error("Q4 event not found.");
+      return;
+    }
+
+    const q4Date = q4Event.Starts;
+
+    startCountdown(q4Date, "q4-days", "q4-hours", "q4-minutes", "q4-seconds");
+  } catch (error) {
+    console.error("Error loading or processing the prom event:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadCountdowns);
+
+async function loadLatinLunches() {
+  try {
+    const response = await fetch("/data/latin_lunch_days.json");
+    const lunches = await response.json();
+
+    const currentDate = new Date();
+
+    const upcomingLunches = lunches.filter((lunch) => {
+      const lunchDate = new Date(lunch.Starts);
+      return lunchDate > currentDate;
+    });
+
+    const latinLunchCounter = document.getElementById("latin-lunch-count");
+    if (upcomingLunches.length > 0) {
+      latinLunchCounter.textContent =
+        upcomingLunches.length + " More Latin Lunches";
+    } else {
+      latinLunchCounter.innerHTML = "<p>No more Latin lunches :(</p>";
+    }
+  } catch (error) {
+    console.error("Error loading Latin lunches:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadLatinLunches);
+
+function calculateDaysOfWeekLeft(graduationEventStarts) {
+  const graduationDate = new Date(graduationEventStarts);
+  const currentDate = new Date();
+
+  let fridaysCount = 0;
+  let mondaysCount = 0;
+
+  let currentDay = new Date(currentDate);
+  currentDay.setDate(currentDay.getDate() + 1);
+  while (currentDay <= graduationDate) {
+    const dayOfWeek = currentDay.getDay();
+    if (dayOfWeek === 5) {
+      fridaysCount++;
+    } else if (dayOfWeek === 1) {
+      mondaysCount++;
+    }
+
+    currentDay.setDate(currentDay.getDate() + 1);
+  }
+
+  const mondayCounter = document.getElementById("monday-count");
+  const fridayCounter = document.getElementById("friday-count");
+
+  if (mondaysCount > 0) {
+    mondayCounter.textContent = mondaysCount + " More Mondays";
+  } else {
+    mondayCounter.textContent = "No more Mondays left!";
+  }
+
+  if (fridaysCount > 0) {
+    fridayCounter.textContent = fridaysCount + " More Fridays";
+  } else {
+    fridayCounter.textContent = "No more Fridays left!";
+  }
+}
