76a4896f25d477fc3852717312bf6c27ee02719b
cqb13 <cqb13.main@gmail.com>
2025-03-12 21:47:17 -0400

    removed pycache

--- filters/build_calendars.py
+++ filters/build_calendars.py
@@ -1,20 +1,13 @@
-import importlib
+import filter
+import get_latin_lunchs
+import get_school_days
 
-modules = ["get_latin_lunchs", "get_school_days", "filter"]
 
-
-def run():
-    for module_name in modules:
-        try:
-            module = importlib.import_module(module_name)
-            if hasattr(module, "run"):
-                print(f"Running {module_name}.run()...")
-                module.run()
-            else:
-                print(f"Warning: {module_name} does not have a 'run' function.")
-        except Exception as e:
-            print(f"Error running {module_name}: {e}")
+def main():
+    filter.run()
+    get_latin_lunchs.run()
+    get_school_days.run()
 
 
 if __name__ == "__main__":
-    run()
+    main()
